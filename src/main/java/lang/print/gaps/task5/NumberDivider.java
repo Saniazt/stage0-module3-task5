0fb058358482cd4fe5f082968d80f30aebefb4f3
saniazt <saniazt@gmail.com>
2022-10-29 22:15:58 +0300

    1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float num = 0;
+        num = toBeDivided / 5;
+        System.out.println(num);
     }
 }
